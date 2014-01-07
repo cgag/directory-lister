4dbe3e504ebf19b82a93cfaae286f41c66cae43e
Curtis Gagliardi <cgag@riseup.net>
2014-01-06 19:30:54 -0800

    Sort files by name.

--- lister.hs
+++ lister.hs
@@ -1,7 +1,7 @@
 import System.Directory (getDirectoryContents, doesFileExist)
 import Control.Monad (filterM)
 import Text.Printf (printf)
-import Data.List (intercalate)
+import Data.List (intercalate, sort)
 import System.Environment (getArgs, getProgName)
 
 getFiles :: FilePath -> IO [FilePath]
@@ -13,9 +13,11 @@ fileToLink path = printf "<a href=\"%s\">%s</a>" path path
 main = do
   progName <- getProgName
   args <- getArgs
-  if length args /= 1 
-    then putStrLn $ printf "Usage: %s <directory>" progName
+  if length args /= 2 
+    then putStrLn $ printf "Usage: %s <directory> <outfile>" progName
     else do
-      let path = head args
-      files <- getFiles path
-      putStrLn $ intercalate "<br />\n" $ map fileToLink files
+      let (dirpath:outfile:_) = args
+      unsorted_files <- getFiles dirpath
+      let files = sort unsorted_files
+      let links = map fileToLink files
+      writeFile outfile $ intercalate "<br />\n" links
